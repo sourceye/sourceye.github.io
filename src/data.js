3c44ad8514acf313157e117dbcaf0514484f1bf0
Theo Dedeken <theo.dedeken@telenet.be>
2023-02-21 22:01:31 +0100

    Add tailwind sorter

--- src/data.js
+++ src/data.js
@@ -14,16 +14,11 @@ export const footerData = {
   links: [
     {
       title: 'Company',
-      links: [
-        { text: 'Blog', href: '/blog' },
-      ],
+      links: [{ text: 'Blog', href: '/blog' }],
     },
   ],
-  secondaryLinks: [
-  ],
-  socialLinks: [
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-  ],
+  secondaryLinks: [],
+  socialLinks: [{ ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') }],
   footNote: `
     Website based on <a class="text-blue-600 hover:underline dark:text-gray-200" href="https://github.com/onwidget/astrowind"> Astrowind</a> template · MIT License.
   `,
