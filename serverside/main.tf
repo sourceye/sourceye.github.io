570306d0a453a7e397792b5817a877a5c534a844
Theo Dedeken <theo.dedeken@telenet.be>
2023-03-19 14:26:49 +0100

    Make function public

--- serverside/main.tf
+++ serverside/main.tf
@@ -67,6 +67,16 @@ resource "google_cloudfunctions_function" "function" {
   }
 }
 
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
 output "function_uri" {
   value = google_cloudfunctions_function.function.https_trigger_url
 }
